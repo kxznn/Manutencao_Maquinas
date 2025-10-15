3a1ebd5fdba3efbbda003bfa5a9e08741f69d387
nicolassmarangoni <kauaroaguiar16@gmail.com>
2025-10-14 22:30:24 -0300

    navbar

--- frontend/front/src/components/navbar.vue
+++ frontend/front/src/components/navbar.vue
@@ -1,16 +1,45 @@
 <template>
-    <section class="navbar">
-        <div class="nav-links">
-            <a href="/home"> Home</a>
-            <a href="/dashboard"> Dashboard </a>
-            <a href="/calendar"> Calnedário </a>
-            <a href="/list"> Lista de Manutenção </a>
-        </div>
-    </section>
+  <header>
+    <div class="logotipo">LOGO</div>
+    <div class="navbar">
+      <nav class="nav-links">
+        <a href="/"> Home</a>
+        <a href="/dashboard"> Dashboard </a>
+        <a href="/calendar"> Calendário </a>
+        <a href="/list"> Lista de Manutenção </a>
+
+        <button href="/login" class="btnLogin"> LOGIN </button>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-    name: 'navbar'
-}
+  name: "navbar",
+};
 </script>
+
+<style scooped>
+header {
+  background-color: rgba(148, 71, 71, 0.658);
+  padding: 1rem;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: white
+}
+
+.nav-links a {
+  text-decoration: none;
+  color: white;
+  margin: 24px;
+}
+
+.btnLogin {
+    padding: 8px;
+    border: 20px 10px;
+    border-radius: 13px;
+}
+</style>
--- frontend/front/src/views/Homepage.vue
+++ frontend/front/src/views/Homepage.vue
@@ -1,8 +1,11 @@
 <template>
-  
+  <navbar />
 </template>
 
 
 <script>
 import navbar from '../components/navbar.vue'
+export default {
+    components: { navbar }
+}
 </script>
