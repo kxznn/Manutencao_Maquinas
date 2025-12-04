229bfbadc10583534c1e996d186f5888ecc28f01
kxaguiar <kauaroaguiar16@gmail.com>
2025-12-03 23:45:47 -0300

    updt front

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,11 @@
 <template>
-  <div class="flex bg-gray-50 min-h-screen font-sans">
-    
-    <Sidebar v-if="$route.name !== 'login'" />
 
-    <main :class="['flex-1', $route.name !== 'login' ? 'ml-64' : '']">
-      <router-view />
-    </main>
-    
-  </div>
+  <router-view />
 </template>
 
-<script setup>
+<script>
+export default {
 
-import Sidebar from '../components/Sidebar.vue'
+  name: 'App'
+}
 </script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Homepage from '../views/Homepage.vue'
+import Homepage from '../views/Homepage.vue'  
 import Dashboard from '../views/DashboardView.vue'          
-import MaintenanceList from '../views/MaintenanceListView.vue'
-import MaintenanceFormView from '../views/MaintenanceFormView.vue' 
+import Maintenancelist from '../views/MaintenanceListView.vue' 
+import MaintenanceFormView from '../views/MaintenanceFormView.vue'
 import CalendarView from '../views/CalendarView.vue'
 import LoginPage from '../views/LoginPage.vue'
 
@@ -13,7 +13,7 @@ const router = createRouter({
     {path: '/', name: 'login', component: LoginPage},
     { path: '/home', name: 'home', component: Homepage }, 
     { path: '/dashboard', name: 'dashboard', component: Dashboard }, 
-    { path: '/lista', name: 'lista', component: MaintenanceList },
+    { path: '/lista', name: 'lista', component: Maintenancelist },
     { path: '/formview', name: 'formview', component: MaintenanceFormView }, 
     { path: '/calendario', name: 'calendario', component: CalendarView },
   ]
