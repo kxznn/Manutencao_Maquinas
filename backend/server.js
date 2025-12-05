38d3ee395da3866ce8f3a856704125c6805e45be
kxznn <kauaroaguiar16@gmail.com>
2025-12-04 22:32:57 -0300

    updt

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use('/api/maintenances', maintenanceRoutes);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
